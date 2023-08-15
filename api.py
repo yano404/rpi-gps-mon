bd3bc4cdbf5e520617c1d0714c114b3c23d8e970
Takayuki Yano <yano.takayuki.55w@st.kyoto-u.ac.jp>
2023-08-15 18:04:26 +0900

    Change env param name

--- api.py
+++ api.py
@@ -8,9 +8,11 @@ from dotenv import load_dotenv
 DOTENV_FILE = pathlib.Path(__file__).resolve().parent.joinpath(".env").resolve()
 load_dotenv(DOTENV_FILE)
 
-DB_NAME = os.environ.get("DB_NAME")
-DB_TABLE_NAME = os.environ.get("DB_TABLE_NAME")
-DB_FILE = pathlib.Path(__file__).resolve().parent.joinpath(f"db/{DB_NAME}").resolve()
+GPS_DB_NAME = os.environ.get("GPS_DB_NAME")
+GPS_DB_TABLE_NAME = os.environ.get("GPS_DB_TABLE_NAME")
+GPS_DB_FILE = (
+    pathlib.Path(__file__).resolve().parent.joinpath(f"db/{GPS_DB_NAME}").resolve()
+)
 
 TEMP_DB_NAME = os.environ.get("TEMP_DB_NAME")
 TEMP_DB_TABLE_NAME = os.environ.get("TEMP_DB_TABLE_NAME")
@@ -20,14 +22,16 @@ TEMP_DB_FILE = (
 
 
 def get_gps_data(start, end):
-    con = sqlite3.connect(str(DB_FILE))
-    statement = f"SELECT timestamp, time, lat, lon, alt, speed FROM {DB_TABLE_NAME} WHERE timestamp > {start} AND timestamp <= {end}"
+    con = sqlite3.connect(str(GPS_DB_FILE))
+    statement = f"""SELECT timestamp, time, lat, lon, alt, speed
+    FROM {GPS_DB_TABLE_NAME} WHERE timestamp > {start} AND timestamp <= {end}"""
     df = pd.read_sql_query(statement, con)
     return df
 
 
 def get_temp_data(start, end):
     con = sqlite3.connect(str(TEMP_DB_FILE))
-    statement = f"SELECT timestamp, degC, degF FROM {TEMP_DB_TABLE_NAME} WHERE timestamp > {start} AND timestamp <= {end}"
+    statement = f"""SELECT timestamp, degC, degF
+    FROM {TEMP_DB_TABLE_NAME} WHERE timestamp > {start} AND timestamp <= {end}"""
     df = pd.read_sql_query(statement, con)
     return df
